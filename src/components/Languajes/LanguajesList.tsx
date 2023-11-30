300732c8b410946e35e4bd5ef6cb7ec3f6b108d1
sjfuentes <sjfuentes@outlook.com>
2023-11-29 21:32:29 -0500

    remove unsued css classes

--- src/components/Languajes/LanguajesList.tsx
+++ src/components/Languajes/LanguajesList.tsx
@@ -4,7 +4,7 @@ function LanguajesList({ languajes }:any) {
   return (
     <>
       <div className="text-center sm:mb-10 md:mb-12 lg:mb-15 mb-10">
-        <h1 className="title-font mb-2 text-3xl font-extrabold leading-10 tracking-tight sm:text-3xl sm:leading-none md:text-4xl lg:text-5xl sm:mb-2 md:mb-3 lg:mb-4">Languajes</h1>
+        <h1 className="mb-2 text-3xl font-bold sm:text-3xl md:text-4xl lg:text-5xl sm:mb-2 md:mb-3 lg:mb-4">Languajes</h1>
         <div className="sm:w-40 md:w-50 lg:w-60 w-40 h-1 rounded-full bg-indigo-500 inline-flex"/>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
