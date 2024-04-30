955ca41dbdf247dc43f2de111b39a55cac1ba087
sjfuentes <sjfuentes@outlook.com>
2024-04-29 20:36:13 -0500

    add filter not workins

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,6 +3,7 @@ import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/project';
 import { Tag } from '../_models/tag';
 import { ProjectsService } from '../_services/projects.service';
+import { flatMap } from 'rxjs';
 
 @Component({
   selector: 'app-portfolio',
@@ -13,11 +14,27 @@ export class PortfolioComponent implements OnInit{
 
   projects = {} as Project[];
 
+  isCollapsed: boolean = true;
+  typescript: boolean = false;
+  angular: boolean = false;
+
   constructor(private titleService: Title, private projectService: ProjectsService) {
     this.titleService.setTitle('Santiago Fuentes - Portfolio')
   }
   
   ngOnInit(): void {
     this.projects = this.projectService.GetProjects();
+  }
+
+  Filter() { 
+    let filterTags: Tag[] = [];
+    if (this.typescript) { 
+      filterTags.push(Tag.TYPESCRIPT);
+    }
+    if (this.angular) { 
+      filterTags.push(Tag.ANGULAR);
+    }
+
+    this.projects = this.projectService.GetProjectsByFilter(filterTags);
   }
 }
